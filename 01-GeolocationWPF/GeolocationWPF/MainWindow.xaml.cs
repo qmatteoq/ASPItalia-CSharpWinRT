07f07f4e4cfab5a340f7d8753ccab44d5a2bb7d7
Matteo Pagani <matteo.pagani@microsoft.com>
2021-03-14 19:34:27 +0100

    Initial commit

---
+++ 01-GeolocationWPF/GeolocationWPF/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows;
+
+
+namespace GeolocationWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnGetPosition(object sender, RoutedEventArgs e)
+        {
+            Windows.Devices.Geolocation.Geolocator geolocator = new();
+            var result = await geolocator.GetGeopositionAsync();
+            txtLatitude.Text = result.Coordinate.Point.Position.Latitude.ToString();
+            txtLongitude.Text = result.Coordinate.Point.Position.Longitude.ToString();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# C#/WinRT 
+This repository includes a series of demos to showcase the usage of the C#/WinRT library. They are a companion to the following article published on ASPItalia.
